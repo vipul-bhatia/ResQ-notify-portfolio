febcb1c78f7899ae1c1612ee68cf6aaed2f9b45e
vipul-bhatia <vipul0592bhatia@gmail.com>
2023-10-14 22:33:33 +0530

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    appDir: true,
-  },
+  // experimental: {
+  //   appDir: true,
+  // },
   images: {
        unoptimized: true,
      },
