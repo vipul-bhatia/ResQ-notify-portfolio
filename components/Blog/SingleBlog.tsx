4ba958aab877bbbf8a4d7a34b87376ea2f6e7b3d
vipul-bhatia <vipul0592bhatia@gmail.com>
2023-10-16 00:14:26 +0530

    .

--- components/Blog/SingleBlog.tsx
+++ components/Blog/SingleBlog.tsx
@@ -10,7 +10,7 @@ const SingleBlog = ({ blog }: { blog: Blog }) => {
         className="wow fadeInUp relative overflow-hidden rounded-md bg-white shadow-one dark:bg-dark"
         data-wow-delay=".1s"
       >
-        <Link href="/" className="relative block h-[220px] w-full">
+        <Link href="https://www.linkedin.com/company/aiprostacksolutions" className="relative block h-[220px] w-full">
           <span className="absolute top-6 right-6 z-20 inline-flex items-center justify-center rounded-full bg-primary py-2 px-4 text-sm font-semibold capitalize text-white">
             {tags[0]}
           </span>
@@ -19,7 +19,7 @@ const SingleBlog = ({ blog }: { blog: Blog }) => {
         <div className="p-6 sm:p-8 md:py-8 md:px-6 lg:p-8 xl:py-8 xl:px-5 2xl:p-8">
           <h3>
             <Link
-              href="/"
+              href="https://www.linkedin.com/company/aiprostacksolutions"
               className="mb-4 block text-xl font-bold text-black hover:text-primary dark:text-white dark:hover:text-primary sm:text-2xl"
             >
               {title}
